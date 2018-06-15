844d04fa53d0dcb3b719c388dc814697a7adc35c
MaidouPP <leept416@gmail.com>
2018-06-15 00:29:14 -0700

    Deleted some unrelated and add .gitignore

--- .python-environments/default/bin/activate.fish
+++ .python-environments/default/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/users/lshixin/.emacs.d/.python-environments/default"
+set -gx VIRTUAL_ENV "/home/shixin/.emacs.d/.python-environments/default"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
