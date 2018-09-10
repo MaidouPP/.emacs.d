3fe8937d86ab6e56f089b447503fc531f2235d40
MaidouPP <leept416@gmail.com>
2018-09-10 11:16:44 -0700

    Added flymd and config for firefox forward

--- custom/setup-md.el
+++ custom/setup-md.el
@@ -8,6 +8,12 @@
          ("\\.md\\'" . markdown-mode)
          ("\\.markdown\\'" . markdown-mode))
   :init (setq markdown-command "multimarkdown")
-        (setq markdown-command "/usr/bin/pandoc"))
+  (setq markdown-command "/usr/bin/pandoc"))
+
+(use-package flymd
+  :init  (defun my-flymd-browser-function (url)
+           (let ((browse-url-browser-function 'browse-url-firefox))
+             (browse-url url)))
+         (setq flymd-browser-open-function 'my-flymd-browser-function))
 
 (provide 'setup-md)
